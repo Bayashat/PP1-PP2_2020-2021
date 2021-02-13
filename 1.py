0a683527c8e0983df2e75f77815f6ec29f2ec7ac
Bayashat <baiashat2002@gmail.com>
2021-02-14 00:45:48 +0600

    change

--- 1.py
+++ 1.py
@@ -1,2 +1,8 @@
-a = "Hello, World!"
-print(a.split(",")) # returns ['Hello', ' World!']
+import math
+res = 0
+for i in range(10):
+    a = 1/(i+1)**2
+    res += a
+res *= 6
+res = math.sqrt(res)
+print(res)
